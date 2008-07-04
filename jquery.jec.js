6ca314797ac8ad11da60d5c908bb8f8cc349fac2
Lukasz.Rajchel <none@none>
2008-07-04 06:02:17 +0000

    fixes #1
    --HG--
    extra : convert_revision : svn%3A65ed09fa-934a-0410-af65-bbc69201c0ed/trunk%4016

--- jquery.jec.js
+++ jquery.jec.js
@@ -1,5 +1,5 @@
 /**
- * jQuery jEC (jQuery Editable Combobox) 0.3.0
+ * jQuery jEC (jQuery Editable Combobox) 0.4.0
  * http://code.google.com/p/jquery-jec
  * http://plugins.jquery.com/project/jEC
  *
@@ -22,99 +22,13 @@
 			classes: '',
 			styles: {},
 			pluginClass: 'jecEditableOption',
-			filters: {
-				96: 0, // numpad 0
-				97: 1, // numpad 1
-				98: 2, // numpad 2
-				99: 3, // numpad 3
-				100: 4, // numpad 4
-				101: 5, // numpad 5
-				102: 6, // numpad 6
-				103: 7, // numpad 7
-				104: 8, // numpad 8
-				105: 9, // numpad 9
-				106: '*', // multiply
-				107: '=', // equal lign
-				109: '-', // subtract
-				110: '.', // decimal point
-				111: '/', // divide
-				186: ';', // semi-colon
-				187: '=', // equal sign
-				188: ',', // comma
-				189: '-', // dash
-				190: '.', // period
-				191: '/', // forward slash
-				192: '`', // grave accent
-				219: '[', // open bracket
-				220: '\\', // back slash
-				221: ']', // close braket
-				222: '\'' // single quote
-			},
-			altFilters: {},
-			ctrlFilters: {},
-			shiftFilters: {
-				48: ')', // right parenthesis
-				49: '!', // exclamation mark
-				50: '@', // at
-				51: '#', // hash
-				52: '$', // dollar sign
-				53: '%', // percent sign
-				54: '^', // caret
-				55: '&', // ampersand
-				56: '*', // asterisk
-				57: '(', // left parenthesis
-				109: '_', // underscore
-				186: ':', // colon
-				188: '<', // left chevron
-				190: '>', // right chevron
-				191: '?', // question mark
-				192: '~', // tilde
-				219: '{', // left brace
-				220: '|', // pipe
-				221: '}', // right brace
-				222: '"' // double quote
-			},
-			altCtrlFilters: {},
-			altShiftFilters: {},
-			ctrlShiftFilters: {},
-			altCtrlShiftFilters: {},
-			ignoredKeys: [
-				9, // tab
-				13, // enter
-				16, // shift
-				17, // ctrl
-				18, // alt
-				19, // pause/break
-				20, // caps lock
-				27, // escape
-				33, // page up
-				34, // page down
-				35, // end
-				36, // home
-				37, // left arrow
-				38, // up arrow
-				39, // right arrow
-				40, // down arrow
-				45, // insert
-				46, // delete
-				91, // left window
-				92, // right window
-				93, // select
-				112, // f1
-				113, // f2
-				114, // f3
-				115, // f4
-				116, // f5
-				117, // f6
-				118, // f7
-				119, // f8
-				120, // f9
-				121, // f10
-				122, // f11
-				123, // f12
-				144, // num lock
-				145 // scroll lock
+			ignoredKeys: [],
+			acceptedRanges: [
+				{min:32, max:126},	// standard chars
+				{min:191, max:382}	// latin accented chars
 			]
+		
+			
 		};
 
 		// options
@@ -123,6 +37,7 @@
 		// IE doesn't implement indexOf() method
 		if ($.browser.msie) {
 			Array.prototype.indexOf = function(object) {
+			
 				for (var i = 0; i < this.length; i++) {
 					if (this[i] == object) {
 						return i;
@@ -164,8 +79,9 @@
 			}
 
 			// handle keys pressed on select
-			$(this).keyup(function(event) {
-				switch(event.keyCode) {
+			$(this).keypress(function(event) {
+
+				switch(event.which) {
 					case 8:	// backspace
 						var option = $(this).children('option.' + options.pluginClass);
 						if (option.val().length > 1) {
@@ -181,34 +97,16 @@
 						break;
 					default:
 						// don't handle ignored keys
-						if (options.ignoredKeys.indexOf(event.keyCode) == -1) {
+						if (options.ignoredKeys.indexOf(event.which) == -1) {
 							// remove selection from all options
 							$(this).children(':selected').removeAttr('selected');
-
-							// filter key code
-							if (event.altKey && event.ctrlKey && event.shiftKey && options.altCtrlShiftFilters[event.keyCode] != null) {
-								keyValue = options.altCtrlShiftFilters[event.keyCode];
-							} else if (event.altKey && event.ctrlKey && options.altCtrlFilters[event.keyCode] != null) {
-								keyValue = options.altCtrlFilters[event.keyCode];
-							} else if (event.altKey && event.shiftKey && options.altShiftFilters[event.keyCode] != null) {
-								keyValue = options.altShiftFilters[event.keyCode];
-							} else if (event.ctrlKey && event.shiftKey && options.ctrlShiftFilters[event.keyCode] != null) {
-								keyValue = options.ctrlShiftFilters[event.keyCode];
-							} else if (event.altKey && options.altFilters[event.keyCode] != null) {
-								keyValue = options.altFilters[event.keyCode];
-							} else if (event.ctrlKey && options.ctrlFilters[event.keyCode] != null) {
-								keyValue = options.ctrlFilters[event.keyCode];
-							} else if (event.shiftKey && options.shiftFilters[event.keyCode] != null) {
-								keyValue = options.shiftFilters[event.keyCode];
-							} else if (options.filters[event.keyCode] != null) {
-								keyValue = options.filters[event.keyCode];
-							} else {
-								// read check if char should be upper or lower case
-								var keyValue = String.fromCharCode(event.keyCode);
-								if (event.shiftKey) {
-									keyValue = keyValue.toUpperCase();
-								} else {
-									keyValue = keyValue.toLowerCase();
+							
+							keyValue = '';
+							// iterate through valid ranges
+							for each (validKey in options.acceptedRanges) {
+								// the range can be either a min,max tuple or exact value
+								if((typeof(validKey.exact) != 'undefined' && validKey.exact == event.which) || (typeof(validKey.min) != 'undefined' && typeof(validKey.max) != 'undefined' && event.which >= validKey.min && event.which <= validKey.max)) {
+									keyValue = String.fromCharCode(event.which);
 								}
 							}
 
@@ -223,7 +121,8 @@
 
 			// disable browser-history-back on backspace
 			$(this).keydown(function(event) {
-				return (event.keyCode != 8);
+			
+				return (event.which != 8);
 			});
 		});
 	};
