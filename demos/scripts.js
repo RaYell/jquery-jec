35ee7b4340b589217bef145f4e61771cfa765b50
Lukasz.Rajchel <none@none>
2009-07-06 08:24:49 +0000

    new demo added
    --HG--
    extra : convert_revision : svn%3A65ed09fa-934a-0410-af65-bbc69201c0ed/trunk%40248

--- demos/scripts.js
+++ demos/scripts.js
@@ -1,39 +1,42 @@
 /*global $,document*/
-/*members "1", "2", "3", after, click, jec, jecOff, jecOn, jecPref, jecValue, ready, tabs, val*/
+/*members "1", "2", "3", after, click, jec, jecOff, jecOn, jecPref, jecValue, ready, tabs, 
+useExistingOptions, val*/
 $(document).ready(function () {
 	$('#tabs').tabs();
 	
 	$('#demo1').jec();
 	
-	$('#demo2').jec();
+	$('#demo2').jec({useExistingOptions: true});
+	
+	$('#demo3').jec();
 	$('#disable').click(function () {
 		if ($(this).val() === 'Disable') {
 			$(this).val('Enable');
-			$('#demo2').jecOff();
+			$('#demo3').jecOff();
 		} else {
 			$(this).val('Disable');
-			$('#demo2').jecOn();
+			$('#demo3').jecOn();
 		}
 	});
 	
-	$('#demo3').jec();
+	$('#demo4').jec();
 	$('#getVal').click(function () {
-		$('#value').val($('#demo3').jecValue());
+		$('#value').val($('#demo4').jecValue());
 	});
 	$('#setVal').click(function () {
-		$('#demo3').jecValue($('#value').val());
+		$('#demo4').jecValue($('#value').val());
 	});
 	
-	$('#demo4').jec();
+	$('#demo5').jec();
 	$('#getPref').click(function () {
-		$('#pref').val($('#demo4').jecPref('position'));
+		$('#pref').val($('#demo5').jecPref('position'));
 	});
 	$('#setPref').click(function () {
-		$('#demo4').jecPref('position', Number($('#pref').val()));
+		$('#demo5').jecPref('position', Number($('#pref').val()));
 	});
 	
 	var options, cb;
 	options =  [{1: 'Alfa Romeo', 2: 'Ferrari', 3: 'Porsche'}];
 	cb = $.jec(options);
-	$('#tabs-5 code').after(cb);
+	$('#tabs-6 code').after(cb);
 });
