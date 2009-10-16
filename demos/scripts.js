72470c831bc573ac39aea4fe1e82b241d6931be2
lukasz@rajchel.pl <lukasz@rajchel.pl>
2009-10-16 08:01:50 +0000

    spaces converted to tabs
    --HG--
    extra : convert_revision : svn%3A65ed09fa-934a-0410-af65-bbc69201c0ed/trunk%40306

--- demos/scripts.js
+++ demos/scripts.js
@@ -5,41 +5,41 @@ bitwise: true, regexp: true, strict: true, newcap: true, immed: true, maxerr: 50
 jecPref, jecValue, useExistingOptions, val*/
 'use strict';
 $(function () {
-	$('#demo1').jec();
-	
-	$('#demo2').jec({useExistingOptions: true});
-	
-	$('#demo3').jec();
-	$('#disable').click(function () {
-		if ($(this).val() === 'Disable') {
-			$(this).val('Enable');
-			$('#demo3').jecOff();
-		} else {
-			$(this).val('Disable');
-			$('#demo3').jecOn();
-		}
-	});
-	
-	$('#demo4').jec();
-	$('#getVal').click(function () {
-		$('#value').val($('#demo4').jecValue());
-	});
-	$('#setVal').click(function () {
-		$('#demo4').jecValue($('#value').val());
-	});
-	
-	$('#demo5').jec();
-	$('#getPref').click(function () {
-		$('#pref').val($('#demo5').jecPref('position'));
-	});
-	$('#setPref').click(function () {
-		$('#demo5').jecPref('position', Number($('#pref').val()));
-	});
-	
-	var options, cb;
-	options =  [{1: 'Alfa Romeo', 2: 'Ferrari', 3: 'Porsche'}];
-	cb = $.jec(options);
-	$('#tabs-6 code').after(cb);
-	
-	$('#demo7').jec({blinkingCursor: true, blinkingCursorInterval: 500});
+    $('#demo1').jec();
+    
+    $('#demo2').jec({useExistingOptions: true});
+    
+    $('#demo3').jec();
+    $('#disable').click(function () {
+        if ($(this).val() === 'Disable') {
+            $(this).val('Enable');
+            $('#demo3').jecOff();
+        } else {
+            $(this).val('Disable');
+            $('#demo3').jecOn();
+        }
+    });
+    
+    $('#demo4').jec();
+    $('#getVal').click(function () {
+        $('#value').val($('#demo4').jecValue());
+    });
+    $('#setVal').click(function () {
+        $('#demo4').jecValue($('#value').val());
+    });
+    
+    $('#demo5').jec();
+    $('#getPref').click(function () {
+        $('#pref').val($('#demo5').jecPref('position'));
+    });
+    $('#setPref').click(function () {
+        $('#demo5').jecPref('position', Number($('#pref').val()));
+    });
+    
+    var options, cb;
+    options =  [{1: 'Alfa Romeo', 2: 'Ferrari', 3: 'Porsche'}];
+    cb = $.jec(options);
+    $('#tabs-6 code').after(cb);
+    
+    $('#demo7').jec({blinkingCursor: true, blinkingCursorInterval: 500});
 });
