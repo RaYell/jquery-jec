b0fdc02f5ac40cba148a99ef5927a46375361652
Lukasz Rajchel <lukasz@rajchel.pl>
2014-05-07 17:16:33 +0200

    coverage treshhold changed from 100% to 90%

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function (grunt) {
                     'test/*.html'
                 ],
                 options: {
-                    threshold: 100,
+                    threshold: 90,
                     run: true
                 }
             }
